4e53b6c90aa5f7bdc37c582f2aabd3e99d34a2ff
Venkata Sai Aavula <venkatasaiaavula@gmail.com>
2025-05-25 16:02:00 -0500

    Project 1 first file books.py

---
+++ books.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/api-endpoint")
+async def fist_api():
+    return {"message": "Hello World"}
+
+
+@app.get("/books")
+async def get_books():
+    return "books"
+
+
+@app.get("/books/my_book")
+async def read_all_books():
+    return {'book_title': "My favourite Book!"}
+
+
+@app.get("/books/{book_title}")
+async def read_books(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
